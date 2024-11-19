f7e64b7afb54a0960e3359a05d842e4362636437
Houlaymatou B <houlaymatoub76@gmail.com>
2024-11-18 22:17:07 -0600

    add scrolling effect to entire page

--- js/script.js
+++ js/script.js
@@ -12,3 +12,30 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// smooth scrolling animation
+const alllinks = document.querySelectorAll("a:link");
+alllinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile nav
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
